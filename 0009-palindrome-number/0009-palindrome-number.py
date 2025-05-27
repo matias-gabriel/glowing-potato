75f1c726cad54f7425fbc5379ea16bb23f1c9c02
Gabriel Matias <matias.santos.mail@gmail.com>
2025-05-27 16:51:21 -0300

    Time: 18 ms (24.2%), Space: 12.5 MB (10.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -7,10 +7,6 @@ class Solution(object):
 
         n_string = str(x)
         len_n_string = len(n_string)
-        # 1 2 1
-        # j: 0 1 2
-        # i: 2 1 0
-
         for i, letter_i in enumerate(n_string):
           j = len_n_string - 1 - i
           if i >= j:
