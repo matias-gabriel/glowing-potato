5e38972e671979848bb61f50f2fba89c981f6509
Gabriel Matias <matias.santos.mail@gmail.com>
2025-05-27 17:36:54 -0300

    Time: 4552 ms (12.99%), Space: 12.6 MB (91.31%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,4 +1,24 @@
 class Solution(object):
+    def checkIsPermutation(self, n, m):
+        # O(n/s1)
+        letters_m = {}
+        letters_n = {}
+
+        for letter in n:
+            if letter in letters_n:
+                letters_n[letter] = letters_n[letter] + 1
+            else: 
+                letters_n[letter]=0
+
+        for letter in m:
+            if letter in letters_m:
+                letters_m[letter] = letters_m[letter] + 1
+            else:
+                letters_m[letter]=0
+
+
+        return letters_m == letters_n
+
     def checkInclusion(self, s1, s2):
         """
         :type s1: str
@@ -8,15 +28,17 @@ class Solution(object):
 
         s1_len = len(s1)
         s2_len = len(s2)
-        sorted_s1 = sorted(s1)
 
+        
+        # O(s2*s1)
         for i, item in enumerate(s2):
             if i + s1_len > s2_len:
                 return False
 
-            sorted_s2_slice = sorted(s2[i:i+s1_len])
+            s2_slice = s2[i:i+s1_len]
 
-            if sorted_s2_slice == sorted_s1:
+            if self.checkIsPermutation(s1,s2_slice):
+                print(s2_slice, s1)
                 return True
         
         return False
