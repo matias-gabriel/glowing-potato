cbdeba91c47b3be536b439edaae54ecb1464b7cd
Gabriel Matias <matias.santos.mail@gmail.com>
2025-06-02 15:09:45 -0300

    Time: 6493 ms (6.25%), Space: 18.4 MB (63.79%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        sorted_array = sorted(nums)
+        result_array = []
+
+        for index, number in enumerate(sorted_array):
+            if index >= len(sorted_array) - 2:
+                break
+            if number > 0:
+                break
+
+            left_pointer = index + 1
+            right_pointer = len(sorted_array) - 1
+
+            # -2, 0, 1, 1 , 2
+            while left_pointer < right_pointer:
+                current_result = [number, sorted_array[left_pointer], sorted_array[right_pointer]]
+                current_sum = sum(current_result)
+                
+                if current_sum == 0:
+                    if current_result not in result_array:
+                        result_array.append(current_result)
+                    right_pointer = right_pointer - 1
+                    left_pointer = left_pointer - -1
+                elif current_sum > 0:
+                    right_pointer = right_pointer - 1
+                else:
+                    left_pointer = left_pointer - -1
+
+        return result_array
+
+
+
+
+                
+        
