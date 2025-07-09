38fc9df98db371815f165446d638631296944739
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-09 16:45:04 +0000

    fix(github-action): format and lint with ruff

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -4,13 +4,10 @@ class Solution:
         repeated = set([])
 
         for i in range(len(s) + 1 - 10):
-            sub_sequence = s[i: i+10]
+            sub_sequence = s[i : i + 10]
             if sub_sequence in sequences_set:
                 repeated.add(sub_sequence)
             else:
                 sequences_set.add(sub_sequence)
 
         return list(repeated)
-
-
-        
