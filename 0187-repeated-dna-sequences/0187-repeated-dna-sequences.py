71451e83dddf5108784c446dd22e4fc3ec077339
matias <matias.santos.mail@gmail.com>
2025-07-09 13:44:48 -0300

    Time: 22 ms (75.13%), Space: 29.2 MB (37.32%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        sequences_set = set([])
+        repeated = set([])
+
+        for i in range(len(s) + 1 - 10):
+            sub_sequence = s[i: i+10]
+            if sub_sequence in sequences_set:
+                repeated.add(sub_sequence)
+            else:
+                sequences_set.add(sub_sequence)
+
+        return list(repeated)
+
+
+        
