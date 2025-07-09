111dae1331f53d6b95b411beac7cc67898605a97
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-09 20:48:37 +0000

    fix(github-action): format and lint with ruff

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,25 +1,28 @@
 import heapq
+
+
 class Solution:
-    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-      # with two heaps
-      affordable_projects_heap = [] # max heap with affordable projects
-      projects_capital_heap = [] # min heap with projects by capital
-      
+    def findMaximizedCapital(
+        self, k: int, w: int, profits: List[int], capital: List[int]
+    ) -> int:
+        # with two heaps
+        affordable_projects_heap = []  # max heap with affordable projects
+        projects_capital_heap = []  # min heap with projects by capital
 
-      for i in range(len(capital)):
-       heapq.heappush(projects_capital_heap, (capital[i], profits[i]))
+        for i in range(len(capital)):
+            heapq.heappush(projects_capital_heap, (capital[i], profits[i]))
 
-      current_k = 0
-      current_capital = w
+        current_k = 0
+        current_capital = w
 
-      for _ in range(k):
-          while projects_capital_heap and projects_capital_heap[0][0] <= current_capital:
-            current_project= heapq.heappop(projects_capital_heap)
-            heapq.heappush(affordable_projects_heap, - current_project[1])
+        for _ in range(k):
+            while (
+                projects_capital_heap and projects_capital_heap[0][0] <= current_capital
+            ):
+                current_project = heapq.heappop(projects_capital_heap)
+                heapq.heappush(affordable_projects_heap, -current_project[1])
 
-          if affordable_projects_heap:
-            current_capital+= - (heapq.heappop(affordable_projects_heap))
+            if affordable_projects_heap:
+                current_capital += -(heapq.heappop(affordable_projects_heap))
 
-      return current_capital
-
-        
+        return current_capital
