47df9c23448bcad1c22db23282740d021de18d22
matias <matias.santos.mail@gmail.com>
2025-07-09 17:48:25 -0300

    Time: 415 ms (31.13%), Space: 42.1 MB (81.8%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,31 +1,25 @@
 import heapq
-
-
 class Solution:
-    def findMaximizedCapital(
-        self, k: int, w: int, profits: List[int], capital: List[int]
-    ) -> int:
-        projects_heap = []
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+      # with two heaps
+      affordable_projects_heap = [] # max heap with affordable projects
+      projects_capital_heap = [] # min heap with projects by capital
+      
 
-        for i in range(len(capital)):
-            heapq.heappush(projects_heap, (-profits[i], capital[i]))
+      for i in range(len(capital)):
+       heapq.heappush(projects_capital_heap, (capital[i], profits[i]))
 
-        current_k = 0
-        initial_capital = w
-        result = w
+      current_k = 0
+      current_capital = w
 
-        return_to_heap = []
-        while current_k != k and len(projects_heap):
-            current_profit, current_capital = heapq.heappop(projects_heap)
+      for _ in range(k):
+          while projects_capital_heap and projects_capital_heap[0][0] <= current_capital:
+            current_project= heapq.heappop(projects_capital_heap)
+            heapq.heappush(affordable_projects_heap, - current_project[1])
 
-            if current_capital <= initial_capital:
-                current_k += 1
-                result += -current_profit
-                initial_capital += -current_profit
+          if affordable_projects_heap:
+            current_capital+= - (heapq.heappop(affordable_projects_heap))
 
-                while len(return_to_heap):
-                    heapq.heappush(projects_heap, return_to_heap.pop())
-            else:
-                return_to_heap.append((current_profit, current_capital))
+      return current_capital
 
-        return result
+        
