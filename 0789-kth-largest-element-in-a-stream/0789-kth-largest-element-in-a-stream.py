cd8a81a6493e81019b64ac296b5cecdeabd5abf4
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-10 19:22:50 +0000

    fix(github-action): format and lint with ruff

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,14 +1,13 @@
 import heapq
-class KthLargest:
 
+
+class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.heap_k_items = []
-        
+
         for num in nums:
             self.add(num)
-
-        
 
     def add(self, val: int) -> int:
         if len(self.heap_k_items) < self.k:
@@ -20,8 +19,6 @@ class KthLargest:
             return self.heap_k_items[0]
         else:
             return self.heap_k_items[0]
-
-        
 
 
 # Your KthLargest object will be instantiated and called as such:
