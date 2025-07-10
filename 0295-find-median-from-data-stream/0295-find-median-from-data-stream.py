a1db30802f58cd7724782e15b5072f54d519b208
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-10 18:20:25 +0000

    fix(github-action): format and lint with ruff

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,53 +1,52 @@
 import heapq
-class MedianFinder:
 
+
+class MedianFinder:
     def __init__(self):
         self.num_of_items = 0
-        self.first_half_heap = [] # (max heap) with first half, all items here < any items in second half heap
+        self.first_half_heap = []  # (max heap) with first half, all items here < any items in second half heap
         self.second_half_heap = []
         # balanced heaps len(min) == len(max) or len(min) - len(max) == 1
         # first [1,0,-1]
-        # second [2,4,5,] 
+        # second [2,4,5,]
 
     def addNum(self, num: int) -> None:
         if not self.first_half_heap:
             heapq.heappush(self.first_half_heap, -num)
         elif not self.second_half_heap:
-            if num <= - self.first_half_heap[0]:
-                first_value = - heapq.heappop(self.first_half_heap)
+            if num <= -self.first_half_heap[0]:
+                first_value = -heapq.heappop(self.first_half_heap)
                 heapq.heappush(self.second_half_heap, first_value)
                 heapq.heappush(self.first_half_heap, -num)
             else:
                 heapq.heappush(self.second_half_heap, num)
 
         else:
-            if num <= - self.first_half_heap[0]:
-                heapq.heappush(self.first_half_heap, - num)
+            if num <= -self.first_half_heap[0]:
+                heapq.heappush(self.first_half_heap, -num)
             else:
                 heapq.heappush(self.second_half_heap, num)
 
-            is_balanced = len(self.first_half_heap) == len(self.second_half_heap) or len(self.first_half_heap) - len(self.second_half_heap) == 1
+            is_balanced = (
+                len(self.first_half_heap) == len(self.second_half_heap)
+                or len(self.first_half_heap) - len(self.second_half_heap) == 1
+            )
 
             if not is_balanced:
                 if len(self.first_half_heap) > len(self.second_half_heap):
-                    value = - heapq.heappop(self.first_half_heap)
+                    value = -heapq.heappop(self.first_half_heap)
                     heapq.heappush(self.second_half_heap, value)
-                
+
                 else:
                     value = heapq.heappop(self.second_half_heap)
-                    heapq.heappush(self.first_half_heap, - value)
-        self.num_of_items+=1 
-
-
-
-
+                    heapq.heappush(self.first_half_heap, -value)
+        self.num_of_items += 1
 
     def findMedian(self) -> float:
         if self.num_of_items % 2 == 0:
-          return (self.second_half_heap[0] - self.first_half_heap[0]) / 2
+            return (self.second_half_heap[0] - self.first_half_heap[0]) / 2
         else:
-           return - self.first_half_heap[0]
-        
+            return -self.first_half_heap[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
