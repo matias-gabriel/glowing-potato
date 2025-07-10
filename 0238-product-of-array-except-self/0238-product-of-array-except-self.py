9b9c7628ea4bf96f580a504f36bc846a1e5071be
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-10 17:40:58 +0000

    fix(github-action): format and lint with ruff

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,7 +5,7 @@ class Solution:
 
         for idx, num in enumerate(nums):
             if num == 0:
-                zeroes_position_set.add(idx) 
+                zeroes_position_set.add(idx)
                 if len(zeroes_position_set) > 1:
                     return [0] * len(nums)
             else:
@@ -23,6 +23,3 @@ class Solution:
             result.append(int(product / i))
 
         return result
-
-                
-
