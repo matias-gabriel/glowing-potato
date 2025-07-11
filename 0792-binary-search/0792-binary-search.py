8c1b844ce8fe516bfd3a681a7d479652b29a4f08
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-11 16:32:40 +0000

    chore(bot): autofix and update docs

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -4,15 +4,12 @@ class Solution:
         end = len(nums)
 
         while start < end:
-            mid = (start + end ) // 2
+            mid = (start + end) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 start = mid + 1
             else:
                 end = mid
-                
 
         return -1
-
-        
