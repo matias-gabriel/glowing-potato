d9a8c6b99e5ad9b9241a4bb6e653302d57c04844
matias <matias.santos.mail@gmail.com>
2025-07-11 16:36:10 -0300

    Time: 4 ms (29.53%), Space: 12.6 MB (75.48%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,3 +1,29 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def generate_permutation(self, nums, indexes, selected, permutations):
+
+        for idx, i in enumerate(nums):
+            if idx in indexes:
+                continue
+
+            indexes.add(idx)
+            selected.append(i)
+            if len(selected) == len(nums):
+                permutations.append(list(selected))
+            else:
+                self.generate_permutation(nums, indexes, list(selected),permutations)
+            selected.pop()
+            indexes.remove(idx)
+
+
+
+                
+
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        permutations = []
+        self.generate_permutation(nums, set([]), [], permutations)
+        return permutations
         
