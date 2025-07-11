f04c421fd8b5988be05061f78d7ab2bd165096ba
matias <matias.santos.mail@gmail.com>
2025-07-11 12:08:33 -0300

    Time: 33 ms (94.18%), Space: 18.5 MB (59.39%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -7,20 +7,21 @@ class Node:
         self.random = random
 """
 
-
 class Node:
-    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
 
-
 class Solution:
-    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        """
         if not head:
             return head
         node_copy_hash = {}
-
         traverse_head = head
 
         while traverse_head:
@@ -30,16 +31,8 @@ class Solution:
         traverse_head = head
         while traverse_head:
             node_copy = node_copy_hash[traverse_head]
-            node_next_copy = (
-                node_copy_hash[traverse_head.next]
-                if traverse_head.next is not None
-                else None
-            )
-            node_next_random = (
-                node_copy_hash[traverse_head.random]
-                if traverse_head.random is not None
-                else None
-            )
+            node_next_copy =  node_copy_hash[traverse_head.next] if traverse_head.next is not None else None
+            node_next_random =  node_copy_hash[traverse_head.random] if traverse_head.random is not None else None
 
             node_copy.next = node_next_copy
             node_copy.random = node_next_random
@@ -47,3 +40,4 @@ class Solution:
             traverse_head = traverse_head.next
 
         return node_copy_hash[head]
+        
