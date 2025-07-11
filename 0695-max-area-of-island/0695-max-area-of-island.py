8e9daba617015023cd6ce4ba63abda3667801276
matias <matias.santos.mail@gmail.com>
2025-07-11 12:25:09 -0300

    Time: 15 ms (89.16%), Space: 18.1 MB (78.68%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def calcIslandArea(self, position, grid):
+      node_queue = deque([position])
+
+      result = 0
+      while node_queue:
+        i,j = node_queue.popleft()
+        if grid[i][j] == 1:
+          grid[i][j] = 0
+          result+=1
+        else:
+          continue
+
+        if i + 1 < len(grid):
+          node_queue.append((i+1, j))
+        if i - 1 >= 0:
+          node_queue.append((i-1, j))
+        if j + 1 < len(grid[i]):
+          node_queue.append((i, j +1))
+        if j - 1 >= 0:
+          node_queue.append((i, j -1))
+
+      return result
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+      result = 0
+      for i, _ in enumerate(grid):
+        for j, value in enumerate(grid[i]):
+          if value == 1:
+            result = max(result, self.calcIslandArea((i,j), grid))
+
+      return result
+
+        
