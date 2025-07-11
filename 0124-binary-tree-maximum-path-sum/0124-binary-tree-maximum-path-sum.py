a6f28350e9caf5a35fb5e3368373e52beb4de670
matias <matias.santos.mail@gmail.com>
2025-07-11 18:33:56 -0300

    Time: 51 ms (5.63%), Space: 23.2 MB (13.73%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def calc_max_path_sum(self, node):
+        if not node:
+            return (0,0)
+
+        # return a tuple
+        # the first position is the max value
+        # traversing only by the left or by the right
+        # the second position is max_path_sum of this subtree
+
+        result_left = self.calc_max_path_sum( node.left)
+        result_right = self.calc_max_path_sum( node.right)
+        
+        current_path = max(node.val, node.val + max(result_left[0], result_right[0])) if (node.left and node.right) else max(node.val + result_left[0] + result_right[0], node.val)
+        
+        current_sum = [node.val, node.val + result_left[0] + result_right[0], node.val + result_left[0], node.val + result_right[0]]
+        if node.left:
+            current_sum.append(result_left[1])
+        if node.right:
+            current_sum.append(result_right[1])
+        
+        print(node.val, current_sum, current_path)
+        return (current_path, max(current_sum))
+
+        
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        return self.calc_max_path_sum(root)[1]
