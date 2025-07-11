4d46ca8dcc3fe89c46fc69d4886f0f1864665328
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-11 20:02:56 +0000

    chore(bot): autofix and update docs

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,21 +6,21 @@
 #         self.right = right
 class Solution:
     def countGoodNodes(self, node, currentCount, maxNode):
-      newCount = 0
-      newMaxNode = maxNode
+        newCount = 0
+        newMaxNode = maxNode
 
-      if not node:
-        return 0
+        if not node:
+            return 0
 
-      if node.val >= maxNode:
-        newCount+=1
-        newMaxNode = node.val
+        if node.val >= maxNode:
+            newCount += 1
+            newMaxNode = node.val
 
-
-      return self.countGoodNodes(node.left, currentCount, newMaxNode) + self.countGoodNodes(node.right, currentCount, newMaxNode) + newCount
-
-
+        return (
+            self.countGoodNodes(node.left, currentCount, newMaxNode)
+            + self.countGoodNodes(node.right, currentCount, newMaxNode)
+            + newCount
+        )
 
     def goodNodes(self, root: TreeNode) -> int:
-      return self.countGoodNodes(root, 0, root.val)
-        
+        return self.countGoodNodes(root, 0, root.val)
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 25          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 26          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 3          |
 |------------------|-----------------|
-| 📊 **Total** | **28** |
+| 📊 **Total** | **29** |
 <!---Exercise counting End-->
 
 </div>
