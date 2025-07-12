0e31dee25188e4a2e0cb67124a51f5d244331dae
matias <matias.santos.mail@gmail.com>
2025-07-11 22:09:09 -0300

    Time: 19 ms (5.22%), Space: 19.5 MB (17.37%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,34 @@
+class Solution:
+    def do_calc(self, n_1, n_2, operation):
+      if operation == "+":
+        return n_1 + n_2
+      elif operation == "-":
+        return n_1 - n_2
+      elif operation == "/":
+        return int(n_1 / n_2)
+      elif operation == "*":
+        return n_1 * n_2
+
+    def is_int(self, i):
+      try:
+        int(i)
+        return True
+      except:
+        False
+
+    def evalRPN(self, tokens: List[str]) -> int:
+      numbers_stack = []
+
+      for i in tokens:
+        if self.is_int(i):
+          numbers_stack.append(int(i))
+        else:
+          n_2 = numbers_stack.pop()
+          n_1 = numbers_stack.pop()
+          value = self.do_calc(n_1, n_2, i)
+
+          print(value)
+          numbers_stack.append(value)
+
+      return numbers_stack.pop()
+        
