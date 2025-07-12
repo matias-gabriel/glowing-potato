5be5ab82d78be49c9ce9d368cf656861e1e4ca81
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-12 00:49:23 +0000

    chore(bot): autofix and update docs

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,36 +1,36 @@
 from collections import deque
 
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rotten_queue = deque([])
         max_minutes = 0
 
-        for i,_ in enumerate(grid):
+        for i, _ in enumerate(grid):
             for j, value in enumerate(grid[i]):
                 if value == 2:
-                    rotten_queue.append((i,j, 0))
+                    rotten_queue.append((i, j, 0))
 
         while rotten_queue:
             current = rotten_queue.popleft()
-            i,j, t = current
+            i, j, t = current
             max_minutes = max(max_minutes, t)
 
             to_visit = []
 
             if i + 1 < len(grid):
-                to_visit.append((i+1, j))
+                to_visit.append((i + 1, j))
             if j + 1 < len(grid[0]):
-                to_visit.append((i, j+1))
+                to_visit.append((i, j + 1))
             if i - 1 >= 0:
-                to_visit.append((i-1, j))
-            if j - 1 >= 0 :
-                to_visit.append((i, j-1))
+                to_visit.append((i - 1, j))
+            if j - 1 >= 0:
+                to_visit.append((i, j - 1))
 
             for v in to_visit:
                 if grid[v[0]][v[1]] == 1:
                     grid[v[0]][v[1]] = 2
                     rotten_queue.append((v[0], v[1], t + 1))
-
 
         for i in grid:
             for j in i:
@@ -38,11 +38,3 @@ class Solution:
                     return -1
 
         return max_minutes
-
-            
-
-
-        
-
-        
-        
