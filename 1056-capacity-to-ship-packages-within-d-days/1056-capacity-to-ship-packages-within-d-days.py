676c4ecc8d105828c619253740cd003515b81e8b
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-13 00:38:09 +0000

    chore(bot): autofix and update docs

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -1,16 +1,17 @@
 import bisect
+
+
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         def check_can_ship(capacity):
             day = 1
             value = 0
             for weight in weights:
-                value+= weight
+                value += weight
 
                 if value > capacity:
-                    day+=1
+                    day += 1
                     value = weight
-                
 
             return day <= days
 
@@ -18,8 +19,3 @@ class Solution:
         right = sum(weights)
 
         return left + bisect.bisect_left(range(left, right), True, key=check_can_ship)
-
-                
-                
-
-        
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 29          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 30          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 3          |
 |------------------|-----------------|
-| 📊 **Total** | **32** |
+| 📊 **Total** | **33** |
 <!---Exercise counting End-->
 
 </div>
