5b7fc07168e8306a81c286d42e4c1c8bb920d66a
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-13 18:14:08 +0000

    chore(bot): autofix and update docs

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,7 +1,7 @@
 class Solution(object):
     # 0  -> 1 k
     # 1 -> (1, 2)
-    def cross(self, current_k, current_stone, last_tone ,stones, memo):
+    def cross(self, current_k, current_stone, last_tone, stones, memo):
         if last_tone == current_stone:
             return True
 
@@ -17,16 +17,30 @@ class Solution(object):
         result_c = False
 
         if current_k >= 0 and memo_a is None:
-            if (current_stone + current_k + 1 in stones):
-                result_a = self.cross(current_k + 1, current_stone + current_k + 1, last_tone ,stones, memo)
-                memo[(current_stone, current_stone + current_k + 1)] = result_a 
-        if current_k >=1 and memo_b is None:
-            if (current_stone + current_k in stones):
-                result_b = self.cross(current_k, current_stone + current_k, last_tone ,stones, memo)
-                memo[(current_stone, current_stone + current_k)] =  result_b
-        if current_k >=2 and memo_c is None:
-            if (current_stone + current_k - 1 in stones):
-                result_c = self.cross(current_k - 1, current_stone + current_k - 1, last_tone ,stones, memo)
+            if current_stone + current_k + 1 in stones:
+                result_a = self.cross(
+                    current_k + 1,
+                    current_stone + current_k + 1,
+                    last_tone,
+                    stones,
+                    memo,
+                )
+                memo[(current_stone, current_stone + current_k + 1)] = result_a
+        if current_k >= 1 and memo_b is None:
+            if current_stone + current_k in stones:
+                result_b = self.cross(
+                    current_k, current_stone + current_k, last_tone, stones, memo
+                )
+                memo[(current_stone, current_stone + current_k)] = result_b
+        if current_k >= 2 and memo_c is None:
+            if current_stone + current_k - 1 in stones:
+                result_c = self.cross(
+                    current_k - 1,
+                    current_stone + current_k - 1,
+                    last_tone,
+                    stones,
+                    memo,
+                )
                 memo[(current_stone, current_stone + current_k - 1)] = result_c
 
         result = result_a or result_b or result_c
@@ -39,8 +53,5 @@ class Solution(object):
         :rtype: bool
         """
         memo = {}
-        res = self.cross(0,stones[0],stones[-1], set(stones), memo)
+        res = self.cross(0, stones[0], stones[-1], set(stones), memo)
         return res
-
-        
-        
