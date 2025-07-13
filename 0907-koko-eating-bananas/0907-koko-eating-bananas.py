7a064c762d7af17f1d0aee64783fc2aecca4686a
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-13 17:25:40 +0000

    chore(bot): autofix and update docs

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,6 +1,7 @@
 import bisect
 import math
 
+
 class Solution:
     # piles [3,6,7,11]
     # 4,1 , 4,2, 2,3 , 4,4, 3,5, 4,6, 4,7, 3,8
@@ -11,23 +12,20 @@ class Solution:
     # if i can eat 4 per hour and finish before or in h=8
     # for sure if I eat more I'll end before taht
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-      min_k = 1
-      max_k = max(piles)
+        min_k = 1
+        max_k = max(piles)
 
-      def check_can_eat(k):
-        hour_count = 0
+        def check_can_eat(k):
+            hour_count = 0
 
-        for pile in piles:
-          hours_needed = math.ceil(pile/k)
-          hour_count+=hours_needed
+            for pile in piles:
+                hours_needed = math.ceil(pile / k)
+                hour_count += hours_needed
 
-          if hour_count > h:
-            return False
-        return hour_count <= h
+                if hour_count > h:
+                    return False
+            return hour_count <= h
 
-      eat_range = range(min_k, max_k+1)
-      result = bisect.bisect_left(eat_range, True, key=check_can_eat)
-      return eat_range[result]
-
-          
-        
+        eat_range = range(min_k, max_k + 1)
+        result = bisect.bisect_left(eat_range, True, key=check_can_eat)
+        return eat_range[result]
