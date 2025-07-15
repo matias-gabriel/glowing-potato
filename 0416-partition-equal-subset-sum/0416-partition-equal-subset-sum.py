9945a2f4965a6037da4278915af40d4420f2e64a
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-15 14:35:28 +0000

    chore(bot): autofix and update docs

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -7,30 +7,31 @@ class Solution:
         elif current > target or idx < 0:
             memo[(idx, current)] = False
             return False
-        
+
         if (idx, current) in memo:
             return memo[(idx, current)]
 
-        result =  self.canPart(idx - 1, target, current + nums[idx-1], nums, memo) or  self.canPart(idx - 1, target, current, nums, memo)
+        result = self.canPart(
+            idx - 1, target, current + nums[idx - 1], nums, memo
+        ) or self.canPart(idx - 1, target, current, nums, memo)
         memo[(idx, current)] = result
         return result
-
 
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2:
             return False
-        
+
         target = sum(nums) // 2
-        dp = [[False]*(target + 1) for _ in range(len(nums) + 1)]
+        dp = [[False] * (target + 1) for _ in range(len(nums) + 1)]
         for i in range(len(nums) + 1):
             dp[i][0] = True
 
         for i in range(1, len(nums) + 1):
             for j in range(1, target + 1):
-                if j < nums[i-1]:
+                if j < nums[i - 1]:
                     dp[i][j] = dp[i - 1][j]
                 else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
-        
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+
         return dp[len(nums)][target]
         # recursion: return self.canPart(len(nums), int(sum(nums) / 2), 0, nums, {})
