361be1ee8629d1bd53b839f0a0945bb895d7e7fa
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-15 18:57:36 +0000

    chore(bot): autofix and update docs

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,40 +1,39 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-      # def r_recursive(idx, sub_arr, memo):
-      #   if idx >= len(sub_arr):
-      #     return 0
-      #   if idx in memo:
-      #     return memo[idx]
-      #
-      #   result = max( r_recursive(idx + 1, sub_arr, memo), sub_arr[idx] + r_recursive(idx + 2, sub_arr, memo))
-      #   memo[idx] = result
-      #
-      #   return memo[idx]
-      #
-      # if len(nums) == 1: return nums[0]
-      # return max(r_recursive(0, nums[:len(nums) - 1], {}), r_recursive(0, nums[1:], {}))
+        # def r_recursive(idx, sub_arr, memo):
+        #   if idx >= len(sub_arr):
+        #     return 0
+        #   if idx in memo:
+        #     return memo[idx]
+        #
+        #   result = max( r_recursive(idx + 1, sub_arr, memo), sub_arr[idx] + r_recursive(idx + 2, sub_arr, memo))
+        #   memo[idx] = result
+        #
+        #   return memo[idx]
+        #
+        # if len(nums) == 1: return nums[0]
+        # return max(r_recursive(0, nums[:len(nums) - 1], {}), r_recursive(0, nums[1:], {}))
 
+        # iterative solution
+        if len(nums) == 1:
+            return nums[0]
 
-      # iterative solution
-      if len(nums) == 1: return nums[0]
+        def r_iterative(sub_array):
+            result_array = [0] * len(sub_array)
 
-      def r_iterative(sub_array):
-        result_array = [0] * len(sub_array)
-  
-        for i in range(len(sub_array)-1, -1, -1):
-          # result_i
-          results_i = []
-          if i + 1 < len(sub_array):
-            results_i.append(result_array[i+1])
-          
-          if i + 2 < len(sub_array):
-            results_i.append(result_array[i+2] + sub_array[i])
-          else:
-            results_i.append(sub_array[i])
+            for i in range(len(sub_array) - 1, -1, -1):
+                # result_i
+                results_i = []
+                if i + 1 < len(sub_array):
+                    results_i.append(result_array[i + 1])
 
-          result_array[i] = max(results_i)
+                if i + 2 < len(sub_array):
+                    results_i.append(result_array[i + 2] + sub_array[i])
+                else:
+                    results_i.append(sub_array[i])
 
-        return result_array[0]
+                result_array[i] = max(results_i)
 
-      return max(r_iterative(nums[:len(nums) - 1]), r_iterative(nums[1:]))
+            return result_array[0]
 
+        return max(r_iterative(nums[: len(nums) - 1]), r_iterative(nums[1:]))
