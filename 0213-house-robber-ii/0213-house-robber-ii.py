cc10ccfe638b9388a693c691fea0bde5f53992aa
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-15 18:49:03 +0000

    chore(bot): autofix and update docs

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-      def r(idx, sub_arr, memo):
-        if idx >= len(sub_arr):
-          return 0
-        if idx in memo:
-          return memo[idx]
-        
-        result = max( r(idx + 1, sub_arr, memo), sub_arr[idx] + r(idx + 2, sub_arr, memo))
-        memo[idx] = result
+        def r(idx, sub_arr, memo):
+            if idx >= len(sub_arr):
+                return 0
+            if idx in memo:
+                return memo[idx]
 
-        return memo[idx]
-      
-      if len(nums) == 1: return nums[0]
-      return max(r(0, nums[:len(nums) - 1], {}), r(0, nums[1:], {}))
+            result = max(
+                r(idx + 1, sub_arr, memo), sub_arr[idx] + r(idx + 2, sub_arr, memo)
+            )
+            memo[idx] = result
+
+            return memo[idx]
+
+        if len(nums) == 1:
+            return nums[0]
+        return max(r(0, nums[: len(nums) - 1], {}), r(0, nums[1:], {}))
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 38          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 39          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 3          |
 |------------------|-----------------|
-| 📊 **Total** | **41** |
+| 📊 **Total** | **42** |
 <!---Exercise counting End-->
 
 </div>
