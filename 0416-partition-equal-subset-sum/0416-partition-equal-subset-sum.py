5efa4586423fa633de1c2bc75a60136865983a44
matias <matias.santos.mail@gmail.com>
2025-07-15 11:35:14 -0300

    Time: 1466 ms (30.51%), Space: 33.4 MB (38.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    # canPart(idx, current) = canPart(idx - 1, current ) or canPart(idx - 1, current + nums[idx1])
     def canPart(self, idx, target, current, nums, memo):
         if target == current and idx > 0:
             memo[(idx, current)] = True
@@ -6,18 +7,30 @@ class Solution:
         elif current > target or idx < 0:
             memo[(idx, current)] = False
             return False
-
+        
         if (idx, current) in memo:
             return memo[(idx, current)]
 
-        result = self.canPart(
-            idx - 1, target, current + nums[idx - 1], nums, memo
-        ) or self.canPart(idx - 1, target, current, nums, memo)
+        result =  self.canPart(idx - 1, target, current + nums[idx-1], nums, memo) or  self.canPart(idx - 1, target, current, nums, memo)
         memo[(idx, current)] = result
         return result
+
 
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2:
             return False
+        
+        target = sum(nums) // 2
+        dp = [[False]*(target + 1) for _ in range(len(nums) + 1)]
+        for i in range(len(nums) + 1):
+            dp[i][0] = True
 
-        return self.canPart(len(nums), int(sum(nums) / 2), 0, nums, {})
+        for i in range(1, len(nums) + 1):
+            for j in range(1, target + 1):
+                if j < nums[i-1]:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
+        
+        return dp[len(nums)][target]
+        # recursion: return self.canPart(len(nums), int(sum(nums) / 2), 0, nums, {})
