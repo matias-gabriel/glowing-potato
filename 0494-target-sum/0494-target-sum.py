69c752f9d1cbf429ce7e878295bee4f7f066379a
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-15 21:55:17 +0000

    chore(bot): autofix and update docs

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,18 +1,19 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-      n = len(nums)
-      def r(idx, target, memo):
-        if idx == n and target == 0:
-          return 1
-        if idx == n:
-          return 0
-        if (idx, target) in memo:
-          return memo[(idx, target)]
+        n = len(nums)
 
-        result = r(idx + 1, target + -1 * nums[idx], memo) + r(idx + 1, target + nums[idx], memo)
-        memo[(idx, target)] = result
-        return result
+        def r(idx, target, memo):
+            if idx == n and target == 0:
+                return 1
+            if idx == n:
+                return 0
+            if (idx, target) in memo:
+                return memo[(idx, target)]
 
-      
+            result = r(idx + 1, target + -1 * nums[idx], memo) + r(
+                idx + 1, target + nums[idx], memo
+            )
+            memo[(idx, target)] = result
+            return result
 
-      return r(0, target, {}) 
+        return r(0, target, {})
