54c7034226c58d3f1694750d82bdca5cb2e2fea5
matias <matias.santos.mail@gmail.com>
2025-07-16 15:55:14 -0300

    Time: 9 ms (65.09%), Space: 18 MB (39.6%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+
+        def r(idx, current, path, result):
+            if idx >= n:
+                return 
+            if current > target:
+                return 
+            if current == target:
+                print(current)
+                result.append(path[:])
+
+
+
+            # r(idx, current) = r(idx+1, current + cand), r(idx, current+cand)
+            for i in range(idx, n):
+                path.append(candidates[i])
+                r(i, current + candidates[i], path, result)
+                path.pop()
+
+
+        result = []
+        r(0,0,[],result)
+        return result
+
+
+
+        
