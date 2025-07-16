bfcbc9e384d18a1c85aa63ae8a12f03161f342d1
matias <matias.santos.mail@gmail.com>
2025-07-16 11:14:06 -0300

    Time: 1944 ms (5.01%), Space: 31.2 MB (19.96%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = len(coins)
+
+        def r(idx, target, memo):
+          if (idx, target) in memo:
+            return memo[(idx, target)]
+          if target == amount and idx < len(coins):
+            return 0
+          if idx >= len(coins) or target > amount:
+            return float('inf')
+
+          result = min(r(idx, target + coins[idx], memo) + 1, r(idx+1, target, memo))
+
+          memo[(idx, target)] = result
+
+          return result
+
+        result = r(0,0,{})
+
+        if result == float('inf'):
+          return -1
+        else:
+          return result
+        
