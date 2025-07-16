faca7f49a78bada69dbd36446686fa403fcd96ca
matias <matias.santos.mail@gmail.com>
2025-07-16 13:07:21 -0300

    Time: 0 ms (100%), Space: 17.7 MB (90.89%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -2,22 +2,25 @@ class Solution:
     # r(idx, result, memo)
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         word_set = set(wordDict)
-
-        def r(idx, current, result, memo):
+        def r(idx, memo):
             if idx == len(s):
-                result.append(" ".join(current))
+                return [""]
             if idx > len(s):
-                return
+                return []
 
+            results = []
             for j in range(idx, len(s)):
-                word = s[idx : j + 1]
+                word = s[idx:j+1]
                 if word in word_set:
-                    current.append(word)
-                    r(j + 1, current, result, memo)
-                    current.pop()
+                    for w in r(j+1,memo):
+                        results.append(f"{word} {w}".strip())
 
-        result = []
 
-        r(0, [], result, {})
+            return results
 
-        return result
+        return r(0, {})
+
+
+                
+
+        
