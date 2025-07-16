f97fd51a4ba8a40ca0f72ad9e061ef068f7198c7
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-16 16:09:39 +0000

    chore(bot): autofix and update docs

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -2,6 +2,7 @@ class Solution:
     # r(idx, result, memo)
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         word_set = set(wordDict)
+
         def r(idx, memo):
             if idx == len(s):
                 return [""]
@@ -10,12 +11,11 @@ class Solution:
             if idx in memo:
                 return memo[idx]
 
-
             results = []
             for j in range(idx, len(s)):
-                word = s[idx:j+1]
+                word = s[idx : j + 1]
                 if word in word_set:
-                    for w in r(j+1,memo):
+                    for w in r(j + 1, memo):
                         results.append(f"{word} {w}".strip())
 
             memo[idx] = results
@@ -23,8 +23,3 @@ class Solution:
             return results
 
         return r(0, {})
-
-
-                
-
-        
