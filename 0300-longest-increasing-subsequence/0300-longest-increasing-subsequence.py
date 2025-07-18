e8f1b6f2a3506e514146db44cad545c230d18dcc
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-18 16:03:13 +0000

    chore(bot): autofix and update docs

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,30 +3,32 @@ class Solution:
         def iterative_lis():
             n = len(nums)
             lis = [0] * (len(nums) + 1)
-    
-            for i in range(1, n+1):
+
+            for i in range(1, n + 1):
                 lis_i = 0
                 for j in range(1, i):
-                    if nums[i-1] < nums[j-1]:
+                    if nums[i - 1] < nums[j - 1]:
                         continue
                     else:
                         curr = lis[j]
-                        if nums[j-1] < nums[i-1]:
-                            curr+=1 
-    
-                    lis_i = max(curr, lis_i )
-    
+                        if nums[j - 1] < nums[i - 1]:
+                            curr += 1
+
+                    lis_i = max(curr, lis_i)
+
                 lis[i] = max(lis_i, 1)
-    
+
             return max(lis)
-                
+
         n = len(nums)
+
         def r(idx, memo):
-            if idx in memo: return memo[idx]
+            if idx in memo:
+                return memo[idx]
             lis = 1
-            for i in range(idx+1, n):
+            for i in range(idx + 1, n):
                 if nums[i] > nums[idx]:
-                    lis = max(r(i, memo) + 1,lis)
+                    lis = max(r(i, memo) + 1, lis)
 
             memo[idx] = lis
             return lis
@@ -37,9 +39,3 @@ class Solution:
             result = max(r(i, memo), result)
 
         return result
-            
-
-
-
-            
-        
