28ed1b4fd5d9e5d955cf8740d268c2ac0b3df73b
matias <matias.santos.mail@gmail.com>
2025-07-18 13:02:58 -0300

    Time: 2832 ms (7.5%), Space: 18.6 MB (9.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,20 +1,45 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        def iterative_lis():
+            n = len(nums)
+            lis = [0] * (len(nums) + 1)
+    
+            for i in range(1, n+1):
+                lis_i = 0
+                for j in range(1, i):
+                    if nums[i-1] < nums[j-1]:
+                        continue
+                    else:
+                        curr = lis[j]
+                        if nums[j-1] < nums[i-1]:
+                            curr+=1 
+    
+                    lis_i = max(curr, lis_i )
+    
+                lis[i] = max(lis_i, 1)
+    
+            return max(lis)
+                
         n = len(nums)
-        lis = [0] * (len(nums) + 1)
+        def r(idx, memo):
+            if idx in memo: return memo[idx]
+            lis = 1
+            for i in range(idx+1, n):
+                if nums[i] > nums[idx]:
+                    lis = max(r(i, memo) + 1,lis)
 
-        for i in range(1, n + 1):
-            lis_i = 0
-            for j in range(1, i):
-                if nums[i - 1] < nums[j - 1]:
-                    continue
-                else:
-                    curr = lis[j]
-                    if nums[j - 1] < nums[i - 1]:
-                        curr += 1
+            memo[idx] = lis
+            return lis
 
-                lis_i = max(curr, lis_i)
+        memo = {}
+        result = 0
+        for i in range(n):
+            result = max(r(i, memo), result)
 
-            lis[i] = max(lis_i, 1)
+        return result
+            
 
-        return max(lis)
+
+
+            
+        
