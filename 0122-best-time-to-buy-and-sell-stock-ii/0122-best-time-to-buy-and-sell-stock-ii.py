e452bd66cf381ddfa3a2bbfc7f52bf9d9a9fb8a0
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-18 14:32:51 +0000

    chore(bot): autofix and update docs

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-      # logic behind that
-      # you can see the future
-      # the logic is, can I profit tommorow [i+1] if I buy today[i]?
+        # logic behind that
+        # you can see the future
+        # the logic is, can I profit tommorow [i+1] if I buy today[i]?
 
-      profit = 0
+        profit = 0
 
-      if len(prices) == 1:
-        return 0
+        if len(prices) == 1:
+            return 0
 
-      for i in range(len(prices)-1):
-        if prices[i] < prices[i+1]:
-          profit+= prices[i+1]-prices[i]
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit += prices[i + 1] - prices[i]
 
-      return profit
-
-
-
+        return profit
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 48          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 49          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 3          |
 |------------------|-----------------|
-| 📊 **Total** | **51** |
+| 📊 **Total** | **52** |
 <!---Exercise counting End-->
 
 </div>
