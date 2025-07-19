f83516f72f2eeee5bfa0ce4c7fcb708789718938
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-19 21:59:21 +0000

    chore(bot): autofix and update docs

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,21 +1,20 @@
 import bisect
-class TimeMap:
 
+
+class TimeMap:
     def __init__(self):
         self.store = {}
-        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if not key in self.store:
             self.store[key] = []
 
-        self.store[key].append((timestamp,value))
-        
+        self.store[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
         if key in self.store:
             idx = bisect.bisect_left(self.store[key], timestamp, key=lambda x: x[0])
-            if (idx == len(self.store[key]) or timestamp != self.store[key][idx][0]) :
+            if idx == len(self.store[key]) or timestamp != self.store[key][idx][0]:
                 value = self.store[key][idx - 1]
                 if timestamp < value[0]:
                     return ""
@@ -25,10 +24,6 @@ class TimeMap:
                 return self.store[key][idx][1]
         else:
             return ""
-
-            
-
-        
 
 
 # Your TimeMap object will be instantiated and called as such:
