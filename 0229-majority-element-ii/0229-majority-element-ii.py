80514580519a2eb5d46b2c1f96bddf3120915b47
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-19 18:21:54 +0000

    chore(bot): autofix and update docs

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,20 +1,19 @@
 import math
 
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
         freq = {}
-        n_max = math.floor(n/3)
+        n_max = math.floor(n / 3)
         result = set([])
 
         for i in nums:
             if i not in freq:
                 freq[i] = 1
             else:
-                freq[i] = freq[i] +1
+                freq[i] = freq[i] + 1
 
             if freq[i] > n_max:
                 result.add(i)
         return list(result)
-
-        
