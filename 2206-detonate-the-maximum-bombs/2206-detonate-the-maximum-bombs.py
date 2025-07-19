2bd731953c91bfcfbf56c7175c648fb504ad1973
matias <matias.santos.mail@gmail.com>
2025-07-19 20:08:50 -0300

    Time: 367 ms (46.05%), Space: 17.8 MB (98.96%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -0,0 +1,48 @@
+from collections import deque
+import math
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        def circles_intersect(xi, yi, ri, xj, yj, rj):
+            dist_sq = (xi - xj)**2 + (yi - yj)**2 
+            return dist_sq <= ri**2
+
+        graph = {}
+        for i, bomb1 in enumerate(bombs):
+            graph[i] = []
+            for j, bomb2 in enumerate(bombs):
+                if i == j:
+                    continue
+                if circles_intersect(bomb1[0], bomb1[1], bomb1[2], bomb2[0], bomb2[1], bomb2[2]):
+                    graph[i].append(j)
+
+
+        result = 0
+        for key in graph.keys():
+            visited = set([key])
+            queue = deque([(key, 1)])
+            while queue:
+                k, w = queue.popleft()
+                for n in graph[k]:
+                    if n not in visited:
+                        visited.add(n)
+                        queue.append((n, w+1)) 
+
+            result = max(result, len(visited))
+
+
+        return result
+
+
+
+
+
+
+        return 0
+
+
+
+
+            
+            
+            
+        
