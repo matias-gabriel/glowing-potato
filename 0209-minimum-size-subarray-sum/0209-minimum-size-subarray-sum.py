6f1dd3b962471d00785dce31dc608b010dbfce9e
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-19 20:13:51 +0000

    chore(bot): autofix and update docs

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -3,7 +3,7 @@ class Solution:
         # idea
         # l, r = 1
         # [2,3,1,2,4,3]
-        #  
+        #
         n = len(nums)
         l = 0
 
@@ -17,13 +17,10 @@ class Solution:
         current_sum = 0
 
         for r in range(n):
-            current_sum+=nums[r]
+            current_sum += nums[r]
             while current_sum >= target:
-                result = min(r-l + 1, result)
-                current_sum-=nums[l]
-                l+=1
-
-
-
+                result = min(r - l + 1, result)
+                current_sum -= nums[l]
+                l += 1
 
         return result if result != float("inf") else 0
