272465ff39993c0a550794643bdb0d306109608e
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-20 21:26:15 +0000

    chore(bot): autofix and update docs

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,35 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-      phone_dict = {
-        2: "abc",
-        3: "def",
-        4: "ghi",
-        5: "jkl",
-        6: "mno",
-        7: "pqrs",
-        8: "tuv",
-        9: "wxyz",
-      }
+        phone_dict = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
+        }
 
-      def r(idx, s, results):
-        if idx == len(digits):
-          results.append(s)
-          return
+        def r(idx, s, results):
+            if idx == len(digits):
+                results.append(s)
+                return
 
-        d = int(digits[idx])
+            d = int(digits[idx])
 
-        for w in phone_dict[d]:
-          r(idx+1, s+w, results)
+            for w in phone_dict[d]:
+                r(idx + 1, s + w, results)
 
-      if not digits:
-        return []
+        if not digits:
+            return []
 
-      results = []
+        results = []
 
-      r(0,"",results)
+        r(0, "", results)
 
-      return results
-
-
-          
-        
+        return results
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 59          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 60          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 3          |
 |------------------|-----------------|
-| 📊 **Total** | **62** |
+| 📊 **Total** | **63** |
 <!---Exercise counting End-->
 
 </div>
