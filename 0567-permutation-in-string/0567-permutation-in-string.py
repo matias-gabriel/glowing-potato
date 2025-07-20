95391479bb721098b1e4423f7cb924e6c1a62466
matias <matias.santos.mail@gmail.com>
2025-07-20 15:00:10 -0300

    Time: 19 ms (66.35%), Space: 17.5 MB (99.88%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,31 +4,27 @@ class Solution:
         hash_s2 = {}
 
         for i in s1:
-            if i not in hash_s1:
-                hash_s1[i] = 0
+          if i not in hash_s1 :
+            hash_s1[i] = 0
 
-            hash_s1[i] = hash_s1[i] + 1
+          hash_s1[i] = hash_s1[i] + 1
 
         n_s1 = len(s1)
         n_s2 = len(s2)
-
+        
         for i in range(n_s2 - n_s1 + 1):
-            if i == 0:
-                for j in s2[0:n_s1]:
-                    if j not in hash_s2:
-                        hash_s2[j] = 0
-                    hash_s2[j] = hash_s2[j] + 1
-            else:
-                print(i - 1, i + n_s1 - 1)
-                print(hash_s2, hash_s1)
-                hash_s2[s2[i - 1]] = hash_s2[s2[i - 1]] - 1
-                if hash_s2[s2[i - 1]] == 0:
-                    del hash_s2[s2[i - 1]]
-                j = i + n_s1 - 1
-                if s2[j] not in hash_s2:
-                    hash_s2[s2[j]] = 0
-                hash_s2[s2[j]] = hash_s2[s2[j]] + 1
-            if hash_s2 == hash_s1:
-                return True
+          if i == 0:
+            for j in s2[0:n_s1]:
+              if j not in hash_s2 :
+                hash_s2[j] = 0
+              hash_s2[j] = hash_s2[j] + 1
+          else:
+            hash_s2[s2[i-1]] = hash_s2[s2[i-1]] - 1
+            if hash_s2[s2[i-1]] == 0: del hash_s2[s2[i-1]]
+            j = i + n_s1 - 1
+            if s2[j] not in hash_s2 :
+              hash_s2[s2[j]] = 0
+            hash_s2[s2[j]] = hash_s2[s2[j]] + 1
+          if hash_s2 == hash_s1: return True
 
         return False
