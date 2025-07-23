f3f4f356be7a644b1ed7898c1720b014833d47a3
matias <matias.santos.mail@gmail.com>
2025-07-23 10:16:38 -0300

    Time: 65 ms (17.77%), Space: 19.6 MB (41.33%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        # lee(t(c)o,de))
+        # ))
+
+        parenthesis_stack = []
+
+        for idx, letter in enumerate(s):
+            if letter not in [")", "("]:
+                continue
+
+            if letter == ")":
+                if len(parenthesis_stack):
+                    last_item = parenthesis_stack[-1][0]
+                    if last_item == "(":
+                        parenthesis_stack.pop()
+                        continue
+            
+            parenthesis_stack.append((letter, idx))
+
+        result = ""
+        not_allowed_idx = set([i[1] for i in parenthesis_stack])
+
+        for idx, s in enumerate(s):
+            if idx in not_allowed_idx:
+                continue
+
+            result+=s
+
+        return result
+
+                    
+
+
+
+        
