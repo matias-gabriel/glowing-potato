8ce9a4b52e79dc7b7f5cc734cf5f7eb61e149adf
matias-gabriel <matias.santos.mail@gmail.com>
2025-07-27 22:02:07 +0000

    chore(bot): autofix and update docs

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,24 +6,27 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-      def r(node, memo):
-        if not node:
-          return 0
+        def r(node, memo):
+            if not node:
+                return 0
 
-        if node in memo:
-          return memo[node]
+            if node in memo:
+                return memo[node]
 
-        node_left_right = r(node.left.right, memo) if node.left else 0
-        node_left_left = r(node.left.left, memo) if node.left else 0
-        node_right_right = r(node.right.right, memo) if node.right else 0
-        node_right_left = r(node.right.left, memo) if node.right else 0
-        result =  max(
-          node.val + node_left_right + node_left_left + node_right_right + node_right_left,
-          r(node.left, memo) + r(node.right, memo),
-        )
+            node_left_right = r(node.left.right, memo) if node.left else 0
+            node_left_left = r(node.left.left, memo) if node.left else 0
+            node_right_right = r(node.right.right, memo) if node.right else 0
+            node_right_left = r(node.right.left, memo) if node.right else 0
+            result = max(
+                node.val
+                + node_left_right
+                + node_left_left
+                + node_right_right
+                + node_right_left,
+                r(node.left, memo) + r(node.right, memo),
+            )
 
-        memo[node] = result
-        return result
+            memo[node] = result
+            return result
 
-      return r(root,{})
-        
+        return r(root, {})
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 68          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 69          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 4          |
 |------------------|-----------------|
-| 📊 **Total** | **72** |
+| 📊 **Total** | **73** |
 <!---Exercise counting End-->
 
 </div>
