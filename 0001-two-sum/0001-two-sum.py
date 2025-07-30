6caf9ffeb084e8de33029ad340fec7f3f0d07a04
matias <matias.santos.mail@gmail.com>
2025-07-30 13:07:01 -0300

    Time: 1 ms (58.31%), Space: 19.1 MB (23.62%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        items_dict = {}
+
+
+        for idx, num in enumerate(nums):
+            if target - num in items_dict:
+                return [idx, items_dict[target-num]]
+            
+            
+            items_dict[num] = idx
+
+        
