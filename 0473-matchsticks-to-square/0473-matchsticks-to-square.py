4a0a9aa5d22ebaa25b11c68f44e48baccdad9b9d
matias <matias.santos.mail@gmail.com>
2025-08-01 10:20:46 -0300

    Time: 4551 ms (8.36%), Space: 393.1 MB (5.02%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -1,11 +1,8 @@
 import math
-
-
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        matchsticks = sorted(matchsticks, reverse=True)
+        matchsticks = sorted(matchsticks, reverse = True)
         max_side = math.ceil(sum(matchsticks) / 4)
-
         def r(position, s1, s2, s3, s4, memo):
             result_tuple = (position, s1, s2, s3, s4)
             if result_tuple in memo:
@@ -13,22 +10,20 @@ class Solution:
             if s1 > max_side or s2 > max_side or s3 > max_side or s4 > max_side:
                 memo[result_tuple] = False
                 return memo[result_tuple]
-
+                
             if position == len(matchsticks):
                 return s1 == s2 == s3 == s4
 
-            if r(position + 1, s1 + matchsticks[position], s2, s3, s4, memo):
-                return True
-            if r(position + 1, s1, s2 + matchsticks[position], s3, s4, memo):
-                return True
-            if r(position + 1, s1, s2, s3 + matchsticks[position], s4, memo):
-                return True
-            if r(position + 1, s1, s2, s3, s4 + matchsticks[position], memo):
-                return True
+
+            if r(position + 1, s1 + matchsticks[position],s2,s3,s4, memo): return True
+            if r(position + 1, s1 ,s2+ matchsticks[position], s3,s4, memo): return True
+            if r(position + 1, s1 ,s2, s3 + matchsticks[position],s4, memo): return True
+            if r(position + 1, s1 ,s2, s3,s4 + matchsticks[position], memo): return True
 
             memo[result_tuple] = False
 
             return False
 
         positions = set([i for i in range(len(matchsticks))])
-        return r(0, 0, 0, 0, 0, {})
+        return r(0, 0,0,0,0, {})
+        
