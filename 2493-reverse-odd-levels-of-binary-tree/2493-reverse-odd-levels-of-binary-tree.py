d8c62be10513c763245469e25095281bf4e9d8a8
matias-gabriel <matias.santos.mail@gmail.com>
2025-08-03 12:07:32 +0000

    chore(bot): autofix and update docs

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -7,19 +7,16 @@
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def invert(level, node_left, node_right):
-            if not node_left or not node_right: return
+            if not node_left or not node_right:
+                return
 
             if (level) % 2 == 0:
                 aux = node_left.val
                 node_left.val = node_right.val
                 node_right.val = aux
 
-            invert(level+1, node_left.left, node_right.right)
-            invert(level+1, node_left.right, node_right.left)
-
+            invert(level + 1, node_left.left, node_right.right)
+            invert(level + 1, node_left.right, node_right.left)
 
         invert(0, root.left, root.right)
         return root
-
-            
-        
--- README.md
+++ README.md
@@ -13,10 +13,10 @@
 <!---Exercise counting Start-->
 | Platform         | Problems Solved |
 |------------------|-----------------|
-| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 72          |
+| 💻 [LeetCode](https://leetcode.com/u/matioias/)      | 73          |
 | 📚 [Educative.io](https://www.educative.io/courses/grokking-coding-interview-in-python)  | 4          |
 |------------------|-----------------|
-| 📊 **Total** | **76** |
+| 📊 **Total** | **77** |
 <!---Exercise counting End-->
 
 </div>
