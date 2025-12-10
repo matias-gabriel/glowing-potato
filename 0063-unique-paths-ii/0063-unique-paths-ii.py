ee4e6bfb2b351138681541b39884ec993198a0e6
matias <matias.santos.mail@gmail.com>
2025-12-09 21:12:52 -0300

    Time: 4 ms (9.23%), Space: 18.1 MB (10.48%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,49 +1,44 @@
-from collections import deque
-
-
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        final = (len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1)
+      final = (len(obstacleGrid) - 1, len(obstacleGrid[0]) -1)
 
-        def possible_directions(current):
-            x, y = current
-            directions = [(0, 1), (1, 0)]
-            options = []
+      def possible_directions(current):
+        x,y = current
+        directions = [(0,1), (1,0)]
+        options = []
+        
+        for idx,direction in enumerate(directions):
+          d_x, d_y = direction
+          i, j = (x + d_x, y + d_y)
 
-            for idx, direction in enumerate(directions):
-                d_x, d_y = direction
-                i, j = (x + d_x, y + d_y)
+          if (0 <= i < len(obstacleGrid)) and (0 <= j < len(obstacleGrid[0])) and obstacleGrid[i][j] != 1:
+            options.append((i,j))
 
-                if (
-                    (0 <= i < len(obstacleGrid))
-                    and (0 <= j < len(obstacleGrid[0]))
-                    and obstacleGrid[i][j] != 1
-                ):
-                    options.append((i, j))
+        return options
+      
+      if obstacleGrid[0][0] == 1 : return 0
 
-            return options
+      memo =  {}
 
-        if obstacleGrid[0][0] == 1:
-            return 0
+      def unique_paths(current):
+        if current == final:
+          return 1
 
-        memo = {}
+        if current in memo: 
+          return memo[current]
 
-        def unique_paths(current):
-            if current == final:
-                return 1
+        value = 0
+        for direction in possible_directions(current):
+          i,j = direction
+          returned = unique_paths((i,j))
 
-            if current in memo:
-                return memo[current]
+          value += returned
 
-            value = 0
-            for direction in possible_directions(current):
-                i, j = direction
-                returned = unique_paths((i, j))
+        memo[current] = value
 
-                value += returned
+        return value
 
-            memo[current] = value
+  
+      return unique_paths((0,0))
 
-            return value
-
-        return unique_paths((0, 0))
+        
